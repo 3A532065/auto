85d0013cdddce9b1460c58a4e4e7bad616e1aa00
auto <weyna1649@gmail.com>
2018-10-15 18:30:51 +0800

    修改三個類別：echo訊息後換行

--- scr/Demo/Hello/Lara.php
+++ scr/Demo/Hello/Lara.php
@@ -6,5 +6,7 @@ class Lara
     function __construct()
     {
         echo 'Hello Lara~'.PHP_EOL;
+        echo 'Hello Lara~<br>'.PHP_EOL;
+
     }
 }
--- scr/Demo/HelloWorld.php
+++ scr/Demo/HelloWorld.php
@@ -6,6 +6,8 @@ class HelloWorld
     function __construct()
     {
         echo 'Hello World~'.PHP_EOL;
+        echo 'Hello World~<br>'.PHP_EOL;
+
     }
 }
 
