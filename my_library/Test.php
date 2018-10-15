321657834bd1d0c30875f8e5694b71e4f73caab6
auto <weyna1649@gmail.com>
2018-10-15 18:27:21 +0800

    自動載入

---
+++ my_library/Test.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 3A532065
+ * Date: 2018/10/15
+ * Time: 下午 06:25
+ */
+
+class Test
+{
+
+}
---
+++ other_library/OtherTest.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 3A532065
+ * Date: 2018/10/15
+ * Time: 下午 06:26
+ */
+class OtherTest
+{
+
+}
